12df2b26f71333165e9485560be0ebda04d53254
hsootree <hsootree@gmail.com>
2017-09-22 11:26:18 +0900

    case await use change useing await

--- src/worker.js
+++ src/worker.js
@@ -8,31 +8,22 @@ const query = require('./query')
 const image = require('./image')
 const queue = kue.createQueue()
 
-queue.process('thumbnail', (job, done) => {
+queue.process('thumbnail', async(job, done) => {
   const { id } = job.data
+  try {
     // 이미지 항목 정보를 데이터베이스에서 가져온 후 (가상서비스의 디스크는 믿을게 못됨.)
-  query.getImageEntryById(id)
-    .then(imageEntry => {
-      // 원본 이미지 다운로드
-      axios.get(imageEntry.original_url, {
-        responseType: 'arrayBuffer' // axios가 똑똑하지 않으니까 어레이버퍼를 사용. arraybuffer 와 buffer의 차이 알아둘 것.
-      }).then(res => {
-        // 썸네일 생성
-        return sharp(res.data)
-          .resize(200, 200)
-          .crop(sharp.gravity.center)
-        res.data
-      }).then(buffer => {
-        // 썸네일 업로드
-
-        return image.uploadImageFile(buffer)
-      }).then(location => {
-        // 이미지 항목의 썸네일 URL 수정
-        return query.updateThumbnailUrlByid(id, ocation)
-      }).then(() => {
-        done()
-      }).catch(err => {
-        done(err)
-      })
+    const imageEntry = await query.getImageEntryById(id)
+    const res = await axios.get(imageEntry.original_url, {
+      responseType: 'arrayBuffer' // axios가 똑똑하지 않으니까 어레이버퍼를 사용. arraybuffer 와 buffer의 차이 알아둘 것.
     })
+    const buffer = await sharp(res.data)
+      .resize(200, 200)
+      .crop(sharp.gravity.center)
+      .toBuffer()
+    const location = await image.uploadImageFile(buffer)
+    await query.updateThumbnailUrlByid(id, location)
+    done()
+  } catch (err) {
+    done(err)
+  }
 })
