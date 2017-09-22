ac4f84787a9478e037164ae25ecabc3946d4dfca
hsootree <hsootree@gmail.com>
2017-09-22 12:21:23 +0900

    modified arrayBuffer to arraybuffer

--- src/worker.js
+++ src/worker.js
@@ -14,7 +14,7 @@ queue.process('thumbnail', async(job, done) => {
     // 이미지 항목 정보를 데이터베이스에서 가져온 후 (가상서비스의 디스크는 믿을게 못됨.)
     const imageEntry = await query.getImageEntryById(id)
     const res = await axios.get(imageEntry.original_url, {
-      responseType: 'arrayBuffer' // axios가 똑똑하지 않으니까 어레이버퍼를 사용. arraybuffer 와 buffer의 차이 알아둘 것.
+      responseType: 'arraybuffer' // axios가 똑똑하지 않으니까 어레이버퍼를 사용. arraybuffer 와 buffer의 차이 알아둘 것.
     })
     const buffer = await sharp(res.data)
       .resize(200, 200)
